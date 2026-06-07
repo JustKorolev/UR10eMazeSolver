1d0472eb356310b2a1470d9b4d4b868bfdaa169a
Andrey Korolev <andreykorolev2004@icloud.com>
2026-06-07 02:04:35 -0700

    yo

--- src/urx_control_thread.py
+++ src/urx_control_thread.py
@@ -234,7 +234,7 @@ class URXControlThread(threading.Thread):
         self.robot.speedj(
             joint_vels.tolist(),
             acc=self.aj,
-            min_time=0.4,
+            min_time=0.05,
         )
 
     def send_zero(self):
